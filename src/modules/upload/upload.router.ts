035ee8b7e1849fadb78ce3a0ae74fdb56c9033a3
Shamim Reza <shamimrezaone@gmail.com>
2026-02-11 21:31:49 +0600

    update

--- src/modules/upload/upload.router.ts
+++ src/modules/upload/upload.router.ts
@@ -5,13 +5,12 @@ import { auth } from '../../middlewares/auth';
 import { CustomFile, upload } from '../../utils/uploadFileSystem';
 import { prisma } from '../../config/prisma';
 import { JwtPayload } from 'jsonwebtoken';
+import { AuthGard } from '../../utils/constant/auth.Constant';
 
 const router = express.Router();
 
 ////////// Upload //////////
-router.post(
-  '/user/resume',
-  auth('USER'),
+router.post('/user/resume',auth(AuthGard.USER),
   upload.single('resume'),
   async (req, res) => {
     const file = req.file as CustomFile; // Cast to CustomFile to access custom properties
@@ -25,17 +24,6 @@ router.post(
     if (!file.documentType) {
       return res.status(400).json({ message: 'Document type is required' });
     }
-
-    // const result = await prisma.document.create({
-    //   data: {
-    //     userId: user.id,
-    //     type: file.documentType as any, // CERTIFICATE
-    //     name: file.documentName, // SSC / HSC / Masters
-    //     path: file.path,
-    //     size: file.size,
-    //     mimeType: file.mimetype,
-    //   },
-    // });
 
     const result = await prisma.$transaction([
       prisma.document.deleteMany({
@@ -62,9 +50,7 @@ router.post(
   },
 );
 
-router.post(
-  '/user/avatar',
-  auth('USER'),
+router.post('/user/avatar', auth('USER'),
   upload.single('avatar'),
   async (req, res) => {
     const file = req.file as CustomFile; // Cast to CustomFile to access custom properties
@@ -78,17 +64,6 @@ router.post(
     if (!file.documentType) {
       return res.status(400).json({ message: 'Document type is required' });
     }
-
-    // const result = await prisma.document.create({
-    //   data: {
-    //     userId: user.id,
-    //     type: file.documentType as any, // CERTIFICATE
-    //     name: file.documentName, // SSC / HSC / Masters
-    //     path: file.path,
-    //     size: file.size,
-    //     mimeType: file.mimetype,
-    //   },
-    // });
 
     const result = await prisma.$transaction([
       prisma.document.deleteMany({
@@ -115,9 +90,7 @@ router.post(
   },
 );
 
-router.post(
-  '/user/signature',
-  auth('USER'),
+router.post('/user/signature', auth('USER'),
   upload.single('signature'),
   async (req, res) => {
     const file = req.file as CustomFile; // Cast to CustomFile to access custom properties
@@ -131,17 +104,6 @@ router.post(
     if (!file.documentType) {
       return res.status(400).json({ message: 'Document type is required' });
     }
-
-    // const result = await prisma.document.create({
-    //   data: {
-    //     userId: user.id,
-    //     type: file.documentType as any, // CERTIFICATE
-    //     name: file.documentName, // SSC / HSC / Masters
-    //     path: file.path,
-    //     size: file.size,
-    //     mimeType: file.mimetype,
-    //   },
-    // });
 
     const result = await prisma.$transaction([
       prisma.document.deleteMany({
@@ -167,7 +129,9 @@ router.post(
     res.json({ message: 'File uploaded successfully', file });
   },
 );
-
 ////////// Upload //////////
+
+
+
 
 export const UploadRouter = router;
